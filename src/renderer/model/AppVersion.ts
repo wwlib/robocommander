942dd3b98759049b7ff0b5e73d2e8d1eb5e20afc
wwlib <andrew@wwlib.org>
2018-08-01 13:25:24 -0400

    0.3.4

--- src/renderer/model/AppVersion.ts
+++ src/renderer/model/AppVersion.ts
@@ -1,2 +1,2 @@
-// This file was generated on Wed Aug  1 13:14:43 EDT 2018
-export const appVersion = '0.3.3';
+// This file was generated on Wed Aug  1 13:25:24 EDT 2018
+export const appVersion = '0.3.4';
