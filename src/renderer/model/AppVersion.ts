37ea30c3298589d77c8e8110c30d55aa6bc6d0a1
Andrew Rapo <andrew@wwlib.org>
2019-03-07 06:36:18 -0500

    0.3.6

--- src/renderer/model/AppVersion.ts
+++ src/renderer/model/AppVersion.ts
@@ -1,2 +1,2 @@
-// This file was generated on Wed Nov 28 11:42:15 EST 2018
-export const appVersion = '0.3.5';
+// This file was generated on Thu Mar  7 06:36:18 EST 2019
+export const appVersion = '0.3.6';
